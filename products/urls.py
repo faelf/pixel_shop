e88333745681a0b38b2f042639909c91e3f81040
Rafael Ferreira <rafaelf_s@hotmail.com>
2025-10-20 00:41:33 +0100

    Implement product detail view and template; update product card links

--- products/urls.py
+++ products/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.products_list, name="products_list"),
+    path("<int:product_id>/", views.product_detail, name="product_detail"),
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
@@ -7,3 +7,8 @@ def products_list(request):
     products = Product.objects.all()
     context = {"products": products}
     return render(request, "products/products_list.html", context)
+
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, "products/product_detail.html", {"product": product})
