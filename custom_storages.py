8705433f7caa59d1b711ee393c74994e721b0eb8
Rafael Ferreira <rafaelf_s@hotmail.com>
2025-10-19 21:34:28 +0100

    Refactor storage classes to use settings for locations

--- custom_storages.py
+++ custom_storages.py
@@ -2,6 +2,9 @@ from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
+class StaticStorage(S3Boto3Storage):
+    location = settings.STATICFILES_LOCATION
+
+
 class MediaStorage(S3Boto3Storage):
-    location = "media"
-    bucket_name = settings.AWS_STORAGE_BUCKET_NAME
+    location = settings.MEDIAFILES_LOCATION
