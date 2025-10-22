a8887b7049d6fa59d1f9fd57eb54c725a8307e50
Rafael Ferreira <rafaelf_s@hotmail.com>
2025-10-22 11:04:24 +0100

    Add HTMX middleware to settings for enhanced AJAX support

--- pixelshop/settings.py
+++ pixelshop/settings.py
@@ -62,6 +62,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_htmx.middleware.HtmxMiddleware",
     # Django Allauth
     "allauth.account.middleware.AccountMiddleware",
 ]
