7de5d5e0ac581a21d77607640c23c8e5cee166b3
Rafael Ferreira <rafaelf_s@hotmail.com>
2025-10-23 12:34:18 +0100

    Implement add to trolley functionality with quantity selection and update trolley view

--- trolley/urls.py
+++ trolley/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import view_trolley
+from .views import view_trolley, add_to_trolley
 
 urlpatterns = [
     path("", view_trolley, name="trolley"),
+    path("add/<item_id>", add_to_trolley, name="add_to_trolley"),
 ]
--- trolley/views.py
+++ trolley/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 # Create your views here.
 def view_trolley(request):
     """A view to return the trolley page"""
     return render(request, "trolley/trolley.html")
+
+
+def add_to_trolley(request, item_id):
+    quantity = int(request.POST.get("quantity"))
+    redirect_url = request.POST.get("redirect_url")
+    trolley = request.session.get("trolley", {})
+
+    if item_id in list(trolley.keys()):
+        trolley[item_id] += quantity
+    else:
+        trolley[item_id] = quantity
+
+    request.session["trolley"] = trolley
+    return redirect(redirect_url)
