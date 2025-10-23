572c906e202adf98c88a6edbee855acc3d41d44a
Rafael Ferreira <rafaelf_s@hotmail.com>
2025-10-23 18:33:54 +0100

    Add URL pattern for message deletion in contact page

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home, contact_page
+from .views import home, contact_page, delete_message
 
 urlpatterns = [
     path("", home, name="home"),
     path("contact/", contact_page, name="contact"),
+    path("contact/delete/<int:message_id>/", delete_message, name="delete_message"),
 ]
