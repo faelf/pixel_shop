c1b0dd9f7dca55b8fd8bf63ceb898d6e01d8f4dd
Rafael Ferreira <rafaelf_s@hotmail.com>
2025-10-23 18:32:48 +0100

    Refactor contact page view to display messages and implement message deletion for staff users

--- home/views.py
+++ home/views.py
@@ -1,14 +1,22 @@
-from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import ContactMessageForm
 from .models import ContactMessage
 
 
 def home(request):
+    """
+    Renders the home page, exteded from base.html.
+    """
     return render(request, "home/home.html")
 
 
 def contact_page(request):
+    """
+    Renders the contact page and handles contact form submissions.
+    The messages will be displayed for staff oldest first.
+    """
     if request.method == "POST":
         form = ContactMessageForm(request.POST)
         if form.is_valid():
@@ -18,7 +26,31 @@ def contact_page(request):
     else:
         form = ContactMessageForm()
 
-    user_messages = ContactMessage.objects.all().order_by("-created_at")
+    user_messages = ContactMessage.objects.all().order_by("created_at")
     context = {"form": form, "user_messages": user_messages}
 
     return render(request, "home/contact.html", context)
+
+
+def is_staff_user(user):
+    """
+    Checks if the user is a staff member.
+    """
+    return user.is_staff
+
+
+@login_required
+@user_passes_test(is_staff_user)
+def delete_message(request, message_id):
+    """
+    Handles the deletion of a single message.
+    Only accessible to staff users.
+    Redirects back to the contact page after deletion.
+    """
+
+    if request.method == "POST":
+        message = get_object_or_404(ContactMessage, pk=message_id)
+        message.delete()
+        messages.success(request, f"Message from {message.name} successfully deleted.")
+        return redirect("contact")
+    return redirect("contact")
