2cbd6efb956a8bbfbc67b95fe256220196764dc4
Rafael Ferreira <rafaelf_s@hotmail.com>
2025-10-27 01:13:26 +0000

    Create checkout form

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import OrderForm
+
 
 # Create your views here.
+def checkout_view(request):
+    """
+    A view to handle the checkout page
+    """
+    trolley = request.session.get("trolley", {})
+    if not trolley:
+        messages.error(request, "Your trolley is empty")
+        return redirect("products_list")
+
+    order_form = OrderForm()
+    template = "checkout/checkout.html"
+    context = {
+        "order_form": order_form,
+        "trolley": trolley,
+    }
+    return render(request, template, context)
--- pixelshop/urls.py
+++ pixelshop/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path("accounts/", include("allauth.urls")),
     path("store/", include("products.urls")),
     path("trolley/", include("trolley.urls")),
+    path("checkout/", include("checkout.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
