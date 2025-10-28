1574e3890715c0c1c9db6efb143a5a59ccc92773
Rafael Ferreira <rafaelf_s@hotmail.com>
2025-10-28 22:34:15 +0000

    Add Edit Product URL

--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.products_list, name="products_list"),
     path("<int:product_id>/", views.product_detail, name="product_detail"),
+    path("product/<int:product_id>/edit/", views.product_edit, name="product_edit"),
 ]
