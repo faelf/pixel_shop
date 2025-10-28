b901e29e3af4e92bd464e0952e6912b5ec98b4a9
Rafael Ferreira <rafaelf_s@hotmail.com>
2025-10-28 22:32:23 +0000

    Create edit product form

---
+++ products/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import Product
+
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ["name", "description", "price", "categories", "image"]
+        widgets = {
+            "name": forms.TextInput(attrs={"class": "form-control"}),
+            "description": forms.Textarea(attrs={"class": "form-control", "rows": 6}),
+            "price": forms.NumberInput(attrs={"class": "form-control", "step": "0.01"}),
+            "categories": forms.CheckboxSelectMultiple(),
+            "image": forms.FileInput(attrs={"class": "form-control"}),
+        }
+        labels = {
+            "categories": "Categories",
+        }
