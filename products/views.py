9f9c0cd8ef41d9ef5a2bfbfd3111e56c5f96aeb0
Rafael Ferreira <rafaelf_s@hotmail.com>
2025-10-29 00:19:28 +0000

    Add description to views

--- products/views.py
+++ products/views.py
@@ -9,6 +9,9 @@ from .models import Product
 
 # Create your views here.
 def products_list(request):
+    """
+    A view to show all products, including sorting and search queries
+    """
     products = Product.objects.all()
 
     search_query = request.GET.get("q", "")
@@ -38,12 +41,18 @@ def products_list(request):
 
 
 def product_detail(request, product_id):
+    """
+    A view to show individual product details
+    """
     product = get_object_or_404(Product, pk=product_id)
     return render(request, "products/product_detail.html", {"product": product})
 
 
 @login_required
 def product_edit(request, product_id):
+    """
+    A view to edit a product
+    """
     product = get_object_or_404(Product, pk=product_id)
 
     if request.method == "POST":
@@ -68,6 +77,9 @@ def product_edit(request, product_id):
 
 @login_required
 def product_add(request):
+    """
+    A view to add a product
+    """
     if request.method == "POST":
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
@@ -82,6 +94,9 @@ def product_add(request):
 
 @login_required
 def product_delete(request, product_id):
+    """
+    A view to delete a product
+    """
     product = get_object_or_404(Product, pk=product_id)
 
     if request.method == "POST":
