29e141e9f0c42a52026163701997fa335a29bbff
Rafael Ferreira <rafaelf_s@hotmail.com>
2025-11-08 22:38:09 +0000

    Refactor code to fit within 80 characters

--- products/forms.py
+++ products/forms.py
@@ -8,8 +8,12 @@ class ProductForm(forms.ModelForm):
         fields = ["name", "description", "price", "categories", "image"]
         widgets = {
             "name": forms.TextInput(attrs={"class": "form-control"}),
-            "description": forms.Textarea(attrs={"class": "form-control", "rows": 6}),
-            "price": forms.NumberInput(attrs={"class": "form-control", "step": "0.01"}),
+            "description": forms.Textarea(
+                attrs={"class": "form-control", "rows": 6}
+            ),
+            "price": forms.NumberInput(
+                attrs={"class": "form-control", "step": "0.01"}
+            ),
             "categories": forms.CheckboxSelectMultiple(),
             "image": forms.FileInput(attrs={"class": "form-control"}),
         }
