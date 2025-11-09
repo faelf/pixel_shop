c5a927504c36d5e5525a33eaf78ce45cc8bc1f39
Rafael Ferreira <rafaelf_s@hotmail.com>
2025-11-09 15:07:55 +0000

    Add send email functionality

--- pixelshop/settings.py
+++ pixelshop/settings.py
@@ -200,7 +200,15 @@ if os.environ.get("USE_AWS") == "True":
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
 ACCOUNT_LOGIN_METHODS = {"username", "email"}
 ACCOUNT_SIGNUP_FIELDS = [
     "email*",
