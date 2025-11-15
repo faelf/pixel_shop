4d35c0ce69f3e1135c9a9881a57ecc2a9695bbc5
Rafael Ferreira <rafaelf_s@hotmail.com>
2025-11-15 22:05:55 +0000

    Fix ordering

--- home/models.py
+++ home/models.py
@@ -9,7 +9,7 @@ class ContactMessage(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ["-created_at"]
+        ordering = ["created_at"]
         verbose_name = "Contact Message"
         verbose_name_plural = "Contact Messages"
 
